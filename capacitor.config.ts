650fc8cfaef9126af6edeb9c50f305625ca79b44
dev <dev@yopmail.com>
2026-03-19 12:13:58 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'psbApp',
+  webDir: 'dist'
+};
+
+export default config;
